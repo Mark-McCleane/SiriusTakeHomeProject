caf0c7676f402653b690321edba912363e0d50da
mcclmar <mccleanemark@gmail.com>
2024-11-30 20:06:33 +0000

    implemented SearchViewModel logic

--- app-compose/src/main/kotlin/com/recipes/app_compose/ui/page/search/SearchScreen.kt
+++ app-compose/src/main/kotlin/com/recipes/app_compose/ui/page/search/SearchScreen.kt
@@ -1,8 +1,14 @@
 package com.recipes.app_compose.ui.page.search
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,21 +24,39 @@ fun SearchScreen(
     // TODO #2: Display a search bar and a list of results.
     //  See [screenshots/compose_search_results.png]
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        Text(
-            text = "Recipes",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.padding(
-                horizontal = HorizontalPadding,
-                vertical = VerticalPadding
+    Scaffold(topBar = {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+        ) {
+            Text(
+                text = "Recipes",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.padding(
+                    horizontal = HorizontalPadding,
+                    vertical = VerticalPadding
+                )
             )
+        }
+    }) { paddingValues ->
+        TextField(
+            value = searchText,
+            onValueChange = viewModel::onSearchTextChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = "Search Tv Show") },
+            trailingIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Tv Shows"
+                    )
+                }
+            }
         )
     }
+
 }
 
 private object SearchScreenDefaults {
--- app-compose/src/main/kotlin/com/recipes/app_compose/ui/page/search/SearchViewModel.kt
+++ app-compose/src/main/kotlin/com/recipes/app_compose/ui/page/search/SearchViewModel.kt
@@ -1,8 +1,16 @@
 package com.recipes.app_compose.ui.page.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.recipes.recipesdk.models.Recipe
+import com.recipes.recipesdk.models.Result
 import com.recipes.recipesdk.repository.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +19,42 @@ class SearchViewModel @Inject constructor(
 ) : ViewModel() {
     // TODO #1: Implement the search screen's ViewModel. The screen should show a blank
     //  page when the query is empty, a loading widget while the page is loading, and a list of
-    //  results when the repository returns data. See the screenshots in /screenshots.
+    //  results when the repository returns data. See the screenshots in /screenshots.#
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery = _searchQuery.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _searchResults = MutableStateFlow<List<Recipe>>(emptyList())
+    val searchResults: StateFlow<List<Recipe>> = _searchResults.asStateFlow()
+
+    private val _errorFlow = MutableStateFlow<String?>(null)
+    val errorFlow: StateFlow<String?> = _errorFlow.asStateFlow()
+
+    init {
+        searchRecipe()
+    }
+
+    private fun searchRecipe(inputText: String = "") {
+        viewModelScope.launch {
+            searchQuery.collect { query ->
+                if (query.isBlank()) {
+                    _searchResults.update { emptyList() }
+                } else {
+                    repository.searchRecipe(query).collect { result ->
+                        if (result is Result.Success) {
+                            _searchResults.update { result.data }
+                        } else if (result is Result.Error) {
+                            _errorFlow.update { result.message }
+                        } else if (result is Result.Loading) {
+                            _isLoading.update { true }
+                        }
+                    }
+                }
+                _isLoading.update { false }
+            }
+        }
+    }
 }
